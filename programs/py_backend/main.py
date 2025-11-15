336f0fa4c17531e8af2cf6fa047ce5ff9c63dd1f
Jordane Thomas <thomasjordane7@gmail.com>
2025-11-15 16:43:48 -0500

    checkpoint: scaffold python backend w/o SQL

---
+++ programs/py_backend/main.py
@@ -0,0 +1,13 @@
+from typing import Any, Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root() -> dict[Any, Any]:
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None) -> dict[str, Any]:
+    return {"item_id": item_id, "q": q}
