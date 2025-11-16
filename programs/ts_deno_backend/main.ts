ec34a0e214df3901369816fdc99be8e04304e0b3
Jordane Thomas <thomasjordane7@gmail.com>
2025-11-16 11:24:42 -0500

    checkpoint: scaffold deno typescript backend w/o SQL

--- programs/ts_deno_backend/main.ts
+++ programs/ts_deno_backend/main.ts
@@ -1,8 +1,13 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import {Application, Router} from "@oak/oak";
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const router = new Router();
+
+router.get("/", (ctx) => {
+  ctx.response.body = "Hello, World!";
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({port: 8004});
